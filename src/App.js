5c369836b1ab96d7c3231034aeeee169e52cf705
Fredrik Skanberg <skanberg@gmail.com>
2017-01-25 16:38:48 +0100

    Added GraphiQL.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.css";
+import "./style.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function graphQLFetcher(graphQLParams) {
+  return fetch(window.location.origin + "/graphql", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
 }
+
+const App = () => (
+  <GraphiQL fetcher={graphQLFetcher} schema={null}>
+    <GraphiQL.Toolbar>
+      <input type="text" />
+    </GraphiQL.Toolbar>
+  </GraphiQL>
+);
 
 export default App;
